a21793ab81433eeba0a5ffb941d2b2c31a6a4d29
Nyanta-23 <muhamadilhan02404@gmail.com>
2023-11-14 17:07:29 +0700

    resolve code

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,6 @@ const todolist = require("./todolist.route");
 route.get("/", (req, res) => {
   // console.log("token :", req.header.authorization);
 
-  const test = req.header.authorization;
-
   res.json({
     message: "Welcome to todolist app",
     test: test,
